adb654a9829ca59b6b399cf254146fbe45013d59
Radwa Mohamed Moustafa <78675194+Radwa-Moustafa@users.noreply.github.com>
2021-03-11 22:32:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '163626818908921',
-        clientSecret: '830fd00d9826dda2e88b461f2760596d'
+        clientId: '',
+        clientSecret: ''
     }
 }
